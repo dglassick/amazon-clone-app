e5a4b7e729feb7f6db5e473e4c4784a3bd081cac
Derek Glassick <derek.glassick@gmail.com>
2020-12-21 11:02:08 -0800

    added Link to the navbar to get to home page

--- amazon-store-clone/src/Components/Header/Header.js
+++ amazon-store-clone/src/Components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
@@ -6,11 +7,13 @@ import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 function Header() {
   return (
     <div className='header'>
-      <img
-        className='header__logo'
-        src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
-        alt='logo'
-      />
+      <Link to='/'>
+        <img
+          className='header__logo'
+          src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
+          alt='logo'
+        />
+      </Link>
       <div className='header__search'>
         <input type='text' className='header__searchInput' />
         <SearchIcon className='header__searchIcon' />
